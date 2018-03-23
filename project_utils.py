ad705f73c83c61aaaac58cee3cf2da6e5076cfd2
Chuan Li <lichuan0620@gmail.com>
2018-03-22 23:15:59 -0700

    continue working on data analysis

--- project_utils.py
+++ project_utils.py
@@ -1,12 +1,16 @@
 import os
-import numpy as np
 import random
+import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 from IPython.display import clear_output
 from skimage.io import imread
 from skimage.transform import resize
 from skimage.color import rgb2gray
+
+np.random.seed()
+random.seed()
 
 
 def hint(message):
@@ -29,14 +33,14 @@ def load_img(path, img_id):
     return imread(path + '/images/' + img_id + '.png')
 
 
-def load_images(path, img_ids, report=True):
+def load_images(path, img_ids, report):
     """given the sample dir path, load the image with the given ids"""
     if report:
         img_ids = tqdm(img_ids)
     return np.array([load_img(path+img_id, img_id) for i, img_id in enumerate(img_ids)])
 
 
-def load_masks(path, img_ids, report=True):
+def load_masks(path, img_ids, report):
     """
     given the sample dir path, load and assemble the masks for the image with the given ids
     """
@@ -45,34 +49,73 @@ def load_masks(path, img_ids, report=True):
     return np.array([load_mask(path+img_id) for img_id in img_ids])
 
 
-def standardize_images(img_list, shape=None, grayscale=False):
+def load_train_set(path, report=True):
     """
-    given a list of images, return a resized version where all the images within have
-    the same shape
+    load the train set from the given path
     """
-    to_return = img_list
-    if grayscale:
-        to_return = [rgb2gray(img) for img in to_return]
-        # to_return = [np.expand_dims(rgb2gray(img), axis=-1) for img in to_return]
-    if shape is not None:
-        to_return = [resize(img, shape, mode='reflect') for img in to_return]
-    return np.array(to_return)
+    to_return = pd.DataFrame(columns=['id', 'image', 'mask'])
+    to_return['id'] = next(os.walk(path))[1]
+    if report:
+        print("Loading images...")
+    to_return['image'] = load_images(path, to_return['id'], report)
+    if report:
+        print("Loading masks...")
+    to_return['mask'] = load_masks(path, to_return['id'], report)
+    return to_return
+
+
+def load_test_set(path, report=True):
+    """
+    load the test set from the given path
+    """
+    to_return = pd.DataFrame(columns=['id', 'image'])
+    to_return['id'] = next(os.walk(path))[1]
+    if report:
+        print("Loading images...")
+    to_return['image'] = load_images(path, to_return['id'], report)
+    return to_return
 
 
 def compare_images(imlist1, imlist2, sample=3, title1='', title2=''):
     """
-    select the given number of pairs of images from the two list and display them
-    side by side
+    display some random samples with the same index number from the two list of images
     """
-    total_sample = imlist1.shape[0]
-    assert total_sample == imlist2.shape[0] and total_sample >= sample
-    fig, axis = plt.subplots(sample, 2, figsize=(12, sample * 6))
+    _, axis = plt.subplots(sample, 2, figsize=(12, sample*6))
     samples = random.sample(range(len(imlist1)), sample)
     for i, sample in enumerate(samples):
         axis[i, 0].imshow(imlist1[sample])
         axis[i, 0].set_title(title1)
         axis[i, 1].imshow(imlist2[sample])
         axis[i, 1].set_title(title2)
+
+
+def get_dim_stat(images):
+    """
+    gather dimension information of the images
+    """
+    to_return = np.zeros((len(images), 4))
+    for i, img in enumerate(images):
+        to_return[i, 0] = img.shape[0]
+        to_return[i, 1] = img.shape[1]
+        to_return[i, 2] = img.shape[0]/img.shape[1]
+        to_return[i, 3] = img.shape[2]
+
+    return pd.DataFrame(
+        to_return,
+        columns=['height', 'width', 'h/w ratio', 'channel count']
+    )
+
+
+def standardize_images(img_list, shape=None, grayscale=False):
+    """
+    given a list of images, return a resized version where all the images within have
+    the same shape
+    load the test set from the given path
+    """
+    to_return = [rgb2gray(img) if grayscale else img[:, :, :3] for img in img_list]
+    if shape is not None:
+        to_return = [resize(img, shape, mode='reflect') for img in to_return]
+    return np.array(to_return)
 
 
 def augment_data(X, Y, vertical_flip=False, horizontal_flip=False, rotate=False):
